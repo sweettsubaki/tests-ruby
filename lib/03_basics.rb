4cdf91d31005f5205278e1faff57ea5ec165d72e
Célia <73950039+sweettsubaki@users.noreply.github.com>
2021-04-19 23:14:00 +0200

    Update 03_basics.rb

--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -1,6 +1,9 @@
 def who_is_bigger(a, b, c)
+#D'abord on nous prévient si l'une des valeurs est "nil" parce qu'on est polis nous /Since we're polite we start by 
+#warning you if one of the values is "nil"
     if a == nil || b == nil || c == nil
         "nil detected"
+        #Ensuite on connait tous les concept des signes inferieurs/superieurs
     else
         if a > b and a > c
             "a is bigger"
@@ -12,17 +15,20 @@ def who_is_bigger(a, b, c)
     end
 end
 
-#Reverse uppercase ! 
+#l'avantage de ruby c'est que tout est dans les noms : Majuscule, inverse, supprime
 def reverse_upcase_noLTA(string)
     string.upcase.reverse.delete("LTA")
 end
 
-#Find 42 number into an array
+#est-ce que l'array contient 42 ? Si oui true, si non false
 def array_42(array)
     array.include?(42) ? true : false
 end
 
-#It's magical
+#flatten : on mélange les 2 array, map ici permet de multiplier par 2 sans changer l'original, delete-if (supprime si) 
+#e est un multiple de 3 parce que si on a par exemple 9/3 ça donne 3x3x3 (il y a donc 0 restes) mais si on fait 14/3 on a 3x3x3x3 et il 
+#nous reste un petit 2 de paumé quelque part (il y a donc un reste). ...oui on dirait un mauvais jeu de mots.
+# Puis on enlève les doublons avec uniq et on trie le tout avec sort
 def magic_array(array)
-    array.flatten.map{|e| e * 2}.delete_if{|x| x % 3 == 0}.uniq.sort
+    array.flatten.map{|d| d * 2}.delete_if{|e| e % 3 == 0}.uniq.sort
 end
