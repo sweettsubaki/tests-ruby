95afe5a19bab961a01db4bffcb8551a2a4a6a2a3
Célia <73950039+sweettsubaki@users.noreply.github.com>
2021-04-19 23:51:33 +0200

    Update 05_timer.rb

--- lib/05_timer.rb
+++ lib/05_timer.rb
@@ -1,3 +1,6 @@
 def time_string(sec)
     Time.at(sec).utc.strftime("%H:%M:%S")
 end
+#on utilise la class Time clairement, qui va partir des secondes, dans le systeme UTC (notre système horaire) sans dépendre 
+#de l'heure du programme qui l'utilise et strftime qui nous permet de choisir l'affichage qu'on veut 
+#(ici on choisit le format 24h séparés par des : )
