7eb295f028d9fb7173dbe88b62371510bb086f50
Célia <73950039+sweettsubaki@users.noreply.github.com>
2021-04-19 21:49:52 +0200

    Update 06_pig_latin.rb

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,1 +1,28 @@
+#Translation
+def translate(string)
+  array = string.split(" ")
+  return array
+end
 
+
+def cut(word)
+  vowels = ["a", "e", "i", "o", "u"]
+
+  if vowels.include? word[0].downcase
+    word + "ay"
+
+
+# for words that start with a single consonant
+  elsif !(vowels.include? word[0])
+    word [1..-1] + word[0] + "ay"
+ # for words that start with 2 consonants
+  elsif !(vowels.include? word[0]) && !(vowels.include? word[1])
+ word[2..-1] + word[0..1] + "ay"
+
+    # for words that start with 3 consonants
+  elsif !(vowels.include? word[0]) && !(vowels.include? word[1]) && !(vowels.include? word [2])
+    word[3..-1] + word[0..2] + "ay"
+
+  end
+
+  #non finit seulement des idées
